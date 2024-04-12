7c51ab91789107c9a193075c4164262a15a4b711
EduardKhusainov <109439388+EduardKhusainov@users.noreply.github.com>
2024-04-12 18:22:52 +0400

    12/04/2024 VFX

---
+++ Assets/GoundCreater.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GoundCreater : MonoBehaviour
+{
+    public GameObject[] go;
+    public Vector3[] offsetPos;
+    public GameObject[] posTriggers;
+
+    public int currentIndex;
+   
+   private void OnTriggerEnter(Collider other) 
+   {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            go[currentIndex].transform.position = go[currentIndex].transform.position + offsetPos[currentIndex];
+            currentIndex++;
+            if(currentIndex<= 2)
+            {
+                transform.position = posTriggers[currentIndex].transform.position;
+            }
+            if(currentIndex >= 3)
+            {
+                currentIndex = 0;
+                transform.position = posTriggers[currentIndex].transform.position;
+            }
+        } 
+   }
+}
