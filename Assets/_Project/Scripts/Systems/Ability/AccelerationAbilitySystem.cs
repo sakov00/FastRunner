9144265dd745b271630ad2e682008a8f932fd07a
Evgeni Sakov <sakov_00@inbox.ru>
2024-05-20 23:24:44 +0300

    Refactoring AccelerationAbilitySystem

--- Assets/_Project/Scripts/Systems/Ability/AccelerationAbilitySystem.cs
+++ Assets/_Project/Scripts/Systems/Ability/AccelerationAbilitySystem.cs
@@ -11,41 +11,57 @@ namespace Assets._Project.Scripts.Systems.Ability
         private readonly EcsFilter<UnitMovementComponent, AbilityComponent, AccelerationAbilityComponent> filter = null;
         public void Run()
         {
-            foreach (var component in filter)
+            foreach (var idx in filter)
             {
-                ref var unitMovement = ref filter.Get1(component);
-                ref var ability = ref filter.Get2(component);
-                ref var accelerationAbility = ref filter.Get3(component);
+                ref var unitMovement = ref filter.Get1(idx);
+                ref var ability = ref filter.Get2(idx);
+                ref var accelerationAbility = ref filter.Get3(idx);
 
                 if (!ability.AccelerationAbilityActivated && !accelerationAbility.IsActive)
-                    return;
+                    continue;
 
                 if (ability.EnergyValue == 0)
                 {
-                    if (accelerationAbility.IsActive)
-                        unitMovement.RunningSpeed -= accelerationAbility.AccelerationAbilityData.ValueSpeedDown;
-                    accelerationAbility.CurrentWorkTime = 0;
-                    accelerationAbility.IsActive = false;
-                    return;
+                    DeactivateAcceleration(ref unitMovement, ref accelerationAbility);
+                    continue;
                 }
 
                 if (accelerationAbility.CurrentWorkTime == 0)
                 {
-                    unitMovement.RunningSpeed += accelerationAbility.AccelerationAbilityData.ValueSpeedUp;
-                    accelerationAbility.IsActive = true;
+                    ActivateAcceleration(ref unitMovement, ref accelerationAbility);
                 }
+
                 if (accelerationAbility.CurrentWorkTime < accelerationAbility.AccelerationAbilityData.EnergyTimer)
                 {
-                    accelerationAbility.CurrentWorkTime += Time.fixedDeltaTime;
-                    ability.EnergyValue -= accelerationAbility.AccelerationAbilityData.EnergyPerSecond * Time.fixedDeltaTime;
+                    MaintainAcceleration(ref ability, ref accelerationAbility);
                 }
-                if (accelerationAbility.CurrentWorkTime > accelerationAbility.AccelerationAbilityData.EnergyTimer)
+                else
                 {
-                    unitMovement.RunningSpeed -= accelerationAbility.AccelerationAbilityData.ValueSpeedDown;
-                    accelerationAbility.CurrentWorkTime = 0;
-                    accelerationAbility.IsActive = false;
+                    DeactivateAcceleration(ref unitMovement, ref accelerationAbility);
                 }
             }
+        }
+
+        private void ActivateAcceleration(ref UnitMovementComponent unitMovement, ref AccelerationAbilityComponent accelerationAbility)
+        {
+            unitMovement.RunningSpeed += accelerationAbility.AccelerationAbilityData.ValueSpeedUp;
+            accelerationAbility.IsActive = true;
+        }
+
+        private void MaintainAcceleration(ref AbilityComponent ability, ref AccelerationAbilityComponent accelerationAbility)
+        {
+            accelerationAbility.CurrentWorkTime += Time.fixedDeltaTime;
+            ability.EnergyValue -= accelerationAbility.AccelerationAbilityData.EnergyPerSecond * Time.fixedDeltaTime;
+        }
+
+        private void DeactivateAcceleration(ref UnitMovementComponent unitMovement, ref AccelerationAbilityComponent accelerationAbility)
+        {
+            if (accelerationAbility.IsActive)
+            {
+                unitMovement.RunningSpeed -= accelerationAbility.AccelerationAbilityData.ValueSpeedDown;
+            }
+            accelerationAbility.CurrentWorkTime = 0;
+            accelerationAbility.IsActive = false;
         }
     }
 }
