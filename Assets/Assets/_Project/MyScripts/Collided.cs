e76a647c4fc1f6ec03200b1cb8fb531cf65860c3
EduardKhusainov <109439388+EduardKhusainov@users.noreply.github.com>
2024-06-04 13:34:20 +0400

    More optimized version

--- Assets/Assets/_Project/MyScripts/Collided.cs
+++ Assets/Assets/_Project/MyScripts/Collided.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,22 @@ public class Collided : MonoBehaviour
 {
     public GameObject canvasWarningImage;
     public StoneSpawner stoneSpawner;
+    public GameObject mapPart;
+    public GameObject activePart;
+    public bool isActive;
     private void OnTriggerEnter(Collider other) 
-    {
-        canvasWarningImage.SetActive(true);
-        Destroy(canvasWarningImage, 3f);
-        other.gameObject.GetComponentInChildren<StoneSpawner>().isEnabled = true;
+    {   
+        if(!isActive)
+        {
+            if(canvasWarningImage != null)
+            canvasWarningImage.SetActive(true);
+            Destroy(canvasWarningImage, 3f);
+            other.gameObject.GetComponentInChildren<StoneSpawner>().isEnabled = true;
+            isActive = true;
+            if(activePart != null)
+            activePart.SetActive(true);
+        }
+        //mapPart.SetActive(false);
+        
     }
 }
