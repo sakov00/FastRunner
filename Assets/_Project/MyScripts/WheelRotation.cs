07f2610e53cf31f39f87ba33d521d87cc188deef
Evgeni Sakov <sakov_00@inbox.ru>
2024-07-27 20:49:27 +0300

    contained prefabs and added functional for objects

---
+++ Assets/_Project/MyScripts/WheelRotation.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class WheelRotation : MonoBehaviour
+{
+    public float rotationSpeed;
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * rotationSpeed);
+    }
+}
